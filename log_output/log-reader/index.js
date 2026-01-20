e455ee7df0a8cc9cb754e09854bf5a403a8949ba
jaylate <178095084+jaylate@users.noreply.github.com>
2026-01-20 18:40:25 +0100

    Done exercise 2.5

--- log_output/log-reader/index.js
+++ log_output/log-reader/index.js
@@ -5,11 +5,20 @@ const PINGS_URL = process.env.PINGS_URL;
 const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  let fileContent = "\r\n";
+  let envVarContent = process.env.MESSAGE + "\r\n";
   let logContent = "\r\n";
   let pingContent = "0\r\n";
   try {
+    fileContent = fs.readFileSync("./config/information.txt", "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+  try {
     logContent = fs.readFileSync("./files/log.txt", "utf8");
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
 
   try {
     let pingRes = await fetch(PINGS_URL);
@@ -21,9 +30,11 @@ const server = http.createServer(async (req, res) => {
     console.log(err);
   }
 
-  console.log(pingContent);
-  res.end(logContent+
-	  "Ping / Pongs: "+pingContent);
+  res.end(
+	  "file content: " + fileContent +
+	  "env variable: MESSAGE=" + envVarContent +
+	  logContent +
+	  "Ping / Pongs: " + pingContent);
 });
 
 const PORT = process.env.PORT || 3000;
